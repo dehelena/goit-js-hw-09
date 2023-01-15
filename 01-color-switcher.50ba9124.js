c68931461f988603cb1d4ca5b7a668997026a89c
dehelena <110464896+dehelena@users.noreply.github.com>
2023-01-15 22:03:59 +0000

    Deploying to gh-pages from @ dehelena/goit-js-hw-09@a5d87eec941382f4a226d4138269ecbd1e999bea 🚀

--- 01-color-switcher.50ba9124.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(function(){console.log("Hello"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.50ba9124.js.map
